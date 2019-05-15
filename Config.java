1f3383ebb9d622889b2ab0297bfd703efc8681a2
MariusKM <m.morhard@hotmail.de>
2019-05-15 17:37:00 +0200

    Feature MaxN : Shallow Pruning + isTerminal in Config

--- Config.java
+++ Config.java
@@ -1,16 +1,15 @@
 package Xbot;
 
-
 class Config implements Cloneable {
 	Stone stones[] = new Stone[28];
 	byte stackSto[] = { 7, 7, 7, 7 };
 	byte ptr = 0; // basically a counter how many stones on board.
-	
+
 	/**
 	 * 
 	 * set up the stones and assign them a player number
 	 */
-	public Config(){
+	public Config() {
 		for (int i = 0; i < this.stones.length; i++) {
 			this.stones[i] = new Stone((byte) -1, (byte) -1, (byte) 0);
 			this.stones[i].inStack = true;
@@ -22,51 +21,78 @@ class Config implements Cloneable {
 
 			} else if (i >= 14 && i < 21) {
 				this.stones[i].player = 2;
-				
+
 			} else if (i >= 21 && i < 28) {
 				this.stones[i].player = 3;
 			}
 		}
 	}
-	
+
+	public boolean isTerminal() {
+
+		for (int i = 0; i < 4; i++) {
+
+			int playerScore = getScore(i);
+
+			if (playerScore >= 7) {
+
+				return true;
+			}
+
+		}
+
+		return false;
+
+	}
+
+	int getScore(int player) {
+		int score = 0;
+		for (int i = player * 7; i < (player + 1) * 7; i++) {
+			if (stones[i].x == 7 || stones[i].y == 7)
+				score++;
+		}
+		return score;
+	}
+
 	@Override
 	public String toString() {
-		String rep="";
+		String rep = "";
 		for (int i = 6; i >= 0; i--) {
-			rep+=i;
-	
-	        for (int j = 0; j <= 6; j++) {
-	        	boolean isthere=false;
-	    		for(Stone st :stones) {
-		            if (i == st.y && j == st.x) {
-		    			rep+=" X";
-		                isthere=true;
-		                break;
-		            }
-	    		}
-	    		if(!isthere){
-	    			rep+=" .";
-	            }
-	        }
-			rep+="\n";
+			rep += i;
+
+			for (int j = 0; j <= 6; j++) {
+				boolean isthere = false;
+				for (Stone st : stones) {
+					if (i == st.y && j == st.x) {
+						rep += " X";
+						isthere = true;
+						break;
+					}
+				}
+				if (!isthere) {
+					rep += " .";
+				}
+			}
+			rep += "\n";
 		}
 		return rep;
-		//return "Config [stones=" + Arrays.toString(stones) + ", stackSto=" + Arrays.toString(stackSto) + ", ptr=" + ptr
-		//		+ "]";
+		// return "Config [stones=" + Arrays.toString(stones) + ", stackSto=" +
+		// Arrays.toString(stackSto) + ", ptr=" + ptr
+		// + "]";
 	}
-	
+
 	@Override
-	protected Config clone(){
+	protected Config clone() {
 		Config c = new Config();
 		c.ptr = this.ptr;
-		
-		for(int i=0; i< stones.length; i++) {
+
+		for (int i = 0; i < stones.length; i++) {
 			c.stones[i] = this.stones[i];
 		}
-		for(int i=0; i< stackSto.length; i++) {
+		for (int i = 0; i < stackSto.length; i++) {
 			c.stackSto[i] = this.stackSto[i];
-		}		
-		
-		return c;		
+		}
+
+		return c;
 	}
 }
--- MinMax.java
+++ MinMax.java
@@ -5,33 +5,32 @@ import java.util.List;
 import lenz.htw.sawhian.Move;
 
 public class MinMax {
-	
+
 	private Move savedMove = null;
 	private int targetDepth = 2;
 	public int[] alive;
-	public Board board; 
-	
-//	public int weight1;
-//	public int weight2;
-//	public int weight3;
-	
-	MinMax(Board config) {		
+	public Board board;
+
+	// public int weight1;
+	// public int weight2;
+	// public int weight3;
+
+	MinMax(Board config) {
 		board = config;
-	
+
 	}
-			
-	public Move run(int player) throws Exception{
+
+	public Move run(int player) throws Exception {
 		int rating = 0;
-		rating = max(player, targetDepth,Integer.MIN_VALUE, Integer.MAX_VALUE);
-		
-		System.out.println("Player"+player+", Rating of the move:" +rating);
-		
+		rating = max(player, targetDepth, Integer.MIN_VALUE, Integer.MAX_VALUE);
+
+		System.out.println("Player" + player + ", Rating of the move:" + rating);
+
 		return savedMove;
 	}
 
-
 	public int maxN(int player, int depth, int alpha) throws Exception {
-		int ratings[]= new int[4];
+		int ratings[] = new int[4];
 		int secondPl = nextPlayer(player);
 		int thirdPl = nextPlayer(secondPl);
 		int fourthPl = nextPlayer(thirdPl);
@@ -44,81 +43,83 @@ public class MinMax {
 			cache = board.getStateConfig().clone();
 			board.makeMove(move);
 			ratings = rateAll();
-			//Maybe Maxsum value different
-			if(ratings[secondPl] == Integer.MAX_VALUE || ratings[thirdPl] == Integer.MAX_VALUE  || ratings[fourthPl] == Integer.MAX_VALUE ) {
-				
+			// Maybe Maxsum value different
+			if (ratings[secondPl] == Integer.MAX_VALUE || ratings[thirdPl] == Integer.MAX_VALUE
+					|| ratings[fourthPl] == Integer.MAX_VALUE) {
+
 			}
-			int wert = maxN(player, depth - 1, alpha);	
-			board.setStateConfig(cache); //macheZugRueckgaengig();
-			//Math.max(a, b)
-			//if() {
+			int wert = maxN(player, depth - 1, alpha);
+			board.setStateConfig(cache); // macheZugRueckgaengig();
+			// Math.max(a, b)
+			// if() {
 			if (depth == targetDepth)
 				savedMove = move;
-		}		
+		}
 		return 0;
 	}
-	
+
 	int max(int player, int depth, int alpha, int beta) throws Exception {
-		// get all free  moves for this configuration
+		// get all free moves for this configuration
 		List<Move> posMoves = board.calcFreeMoves(player, board);
 		if (depth == 0 || posMoves == null)
 			return rate(player);
 		Config cache;
-		int maxWert = alpha; //ab
-		//Max as the next player in line 
+		int maxWert = alpha; // ab
+		// Max as the next player in line
 		player = nextPlayer(player);
 		for (Move move : posMoves) {
 			cache = board.getStateConfig().clone();
 			board.makeMove(move);
-			int wert = max(player, depth - 1, maxWert, beta);	
-			board.setStateConfig(cache); //macheZugRueckgaengig();
-			
+			int wert = max(player, depth - 1, maxWert, beta);
+			board.setStateConfig(cache); // macheZugRueckgaengig();
+
 			if (wert > maxWert) {
-				maxWert = wert; 
-				if(maxWert >= beta )break; //ab
+				maxWert = wert;
+				if (maxWert >= beta)
+					break; // ab
 				if (depth == targetDepth)
 					savedMove = move;
 			}
-		}		
+		}
 		return maxWert;
 	}
 
 	private int min(int player, int depth, int alpha, int beta) throws Exception {
-		List<Move> posMoves =  board.calcFreeMoves(player, board);
+		List<Move> posMoves = board.calcFreeMoves(player, board);
 		if (depth == 0 || posMoves == null)
 			return rate(player);
-		//Board cache;
+		// Board cache;
 		Config cache;
 		int minWert = beta;
 		player = nextPlayer(player);
 		for (Move move : posMoves) {
-			cache = board.getStateConfig().clone();			
+			cache = board.getStateConfig().clone();
 			board.makeMove(move);
 			int wert = 0;
-			wert = minMin(player, depth - 1, minWert, beta); 
-			board.setStateConfig(cache); //macheZugRueckgaengig();
+			wert = minMin(player, depth - 1, minWert, beta);
+			board.setStateConfig(cache); // macheZugRueckgaengig();
 			if (wert < minWert) {
 				minWert = wert;
 			}
 		}
 		return minWert;
 	}
-	
-	private int minMin(int player, int depth, int alpha, int beta) throws Exception { //NOT
+
+	private int minMin(int player, int depth, int alpha, int beta) throws Exception { // NOT
 		List<Move> posMoves = board.calcFreeMoves(player, board);
 		if (depth == 0 || posMoves == null)
 			return rate(player);
-		//Board cache;
+		// Board cache;
 		Config cache;
 		int minWert = Integer.MAX_VALUE;
 		player = nextPlayer(player);
 		for (Move move : posMoves) {
-			cache = board.getStateConfig().clone();			
+			cache = board.getStateConfig().clone();
 			board.makeMove(move);
 			int wert = 0;
 			wert = max(player, depth - 1, alpha, minWert);
-			board.setStateConfig(cache); //macheZugRueckgaengig();
-			
+			board.setStateConfig(cache); // macheZugRueckgaengig();
+
 			if (wert < minWert) {
 				minWert = wert;
 			}
@@ -127,124 +128,143 @@ public class MinMax {
 	}
 
 	int nextPlayer(int player) {
-		if(player < 4) {
+		if (player < 4) {
 			player++;
-		}else {
-			player=0;
+		} else {
+			player = 0;
 		}
 		return player;
 	}
-	
 
-	int[] rateAll(){
-		int ratings[]= new int[4];
-		ratings[0]=rate(0);
-		ratings[1]=rate(1);
-		ratings[2]=rate(2);
-		ratings[3]=rate(3);
+	int[] rateAll() {
+		int ratings[] = new int[4];
+		ratings[0] = rate(0);
+		ratings[1] = rate(1);
+		ratings[2] = rate(2);
+		ratings[3] = rate(3);
 		return ratings;
 	}
-	
-	int rate(int player){
+
+	int rate(int player) {
 		int freeStones = 0;
 		int jumps = 0;
 		Config conf = board.getStateConfig();
 		Board bo = board;
 		for (int i = player * 5; i < (player + 1) * 5; i++) { // for all stones of this player
-			if (conf.stones[i].inStack && (conf.stones[i].x != 7 || conf.stones[i].x != 7)) { //Stone not in stack or 
+			if (conf.stones[i].inStack && (conf.stones[i].x != 7 || conf.stones[i].x != 7)) { // Stone not in stack or
 				freeStones++;
 				jumps += conf.stones[i].canJump(bo);
 			}
 		}
-		if(freeStones == 0 ) {
+		if (freeStones == 0) {
 			int secondPlayer = nextPlayer(player);
 			int thirdPlayer = nextPlayer(secondPlayer);
 			int fourthPlayer = nextPlayer(thirdPlayer);
-			
-			if(bo.getScore(player) < bo.getScore(secondPlayer) || bo.getScore(player) < bo.getScore(thirdPlayer) || bo.getScore(player) < bo.getScore(fourthPlayer) ) {
-				//if anybody has a higher score
-				return Integer.MIN_VALUE; //lost
-			}else if(bo.getScore(player) > bo.getScore(secondPlayer) && bo.getScore(player) > bo.getScore(thirdPlayer) &&  bo.getScore(player) > bo.getScore(fourthPlayer) ) {
-				//if i have the highest score
-				return Integer.MAX_VALUE; //Won
-			}else if(bo.getScore(player) ==  bo.getScore(secondPlayer) && bo.getScore(thirdPlayer) == bo.getScore(fourthPlayer) ) {
-				//TODO finish check
-				//Check all scores with each other
-				return 0; //Draw
+
+			if (bo.getScore(player) < bo.getScore(secondPlayer) || bo.getScore(player) < bo.getScore(thirdPlayer)
+					|| bo.getScore(player) < bo.getScore(fourthPlayer)) {
+				// if anybody has a higher score
+				return Integer.MIN_VALUE; // lost
+			} else if (bo.getScore(player) > bo.getScore(secondPlayer) && bo.getScore(player) > bo.getScore(thirdPlayer)
+					&& bo.getScore(player) > bo.getScore(fourthPlayer)) {
+				// if i have the highest score
+				return Integer.MAX_VALUE; // Won
+			} else if (bo.getScore(player) == bo.getScore(secondPlayer)
+					&& bo.getScore(thirdPlayer) == bo.getScore(fourthPlayer)) {
+				// TODO finish check
+				// Check all scores with each other
+				return 0; // Draw
 			}
 		}
-		return  freeStones+jumps;
-	 }
-	
-}
-	
+		return freeStones + jumps;
+	}
 
-	/**
-	 * Basically like wikipedia, except player number is increased
-	 * @param player
-	 * @param depth
-	 * @param alpha
-	 * @param beta
-	 * @return
-	 *//*
-	int NegaMaxAB(int player, int depth, int alpha, int beta) {
-		List<Move> posMoves = copyBoard.calcFreeMoves(player,copyBoard);
-		if (depth == 0 || posMoves == null)
-			return rate2Pl(player);
-	    int maxWert = alpha;
-	    Board cache;
+	int[] Shallow(Config Node, int Player, int Bound) {
+		// add something here so that we can check if a player has a score = 7
+
+		int maxSum = 25;
+
+		if (Node.isTerminal()) {
+
+			/// this should indicate best or worst case scenarios ( iE win or lose)
+			return rateAll();
+		}
+		// get all possible moves that can be made for this player
+		List<Move> posMoves = board.calcFreeMoves(Player, board);
+		// get the first child node
+		Move firstMove = posMoves.get(0);
+		// create cache and make the first possible move
+		Config cache;
+		cache = board.getStateConfig().clone();
+		board.makeMove(firstMove);
+		// populate the ratings table by evaluatibg the first leaf node
+		int Best[] = Shallow(board.getStateConfig(), nextPlayer(Player), maxSum);
+		// reset the board so that it can be used again
+		board.setStateConfig(cache);
+		int i = 0;
+		// now for the remaining child nodes
 		for (Move move : posMoves) {
-	    	cache = copyBoard.clone();
-			copyBoard.update(move);
-	        int wert = -NegaMaxAB(-player, depth-1, -beta, -maxWert);	
-			copyBoard = cache;
-	       if (wert > maxWert) {
-	          maxWert = wert;
-	          if (maxWert >= beta)
-	             break;
-	          if (depth == targetDepth)
-	             savedMove = move;
-	       }
-	    }
-	    return maxWert;
+			if (i != 0) {
+
+				if (Best[Player]>= Bound) {
+
+					return Best;
+				}
+
+				cache = board.getStateConfig().clone();
+				board.makeMove(firstMove);
+				int Current[] = Shallow(board.getStateConfig(), nextPlayer(Player), maxSum);
+				if (Current[Player] >= Best[Player]) {
+
+					Best = Current;
+				}
+				// reset the board so that it can be used again
+				board.setStateConfig(cache);
+			}
+
+			i++;
+
+		}
+
+		return Best;
+
 	}
-	
-	/*
-	int AlphaBeta(int depth, int alpha, int beta)
-	{
-	    if (depth == 0)
-	        return rate();
-	    boolean PVgefunden = false;
-	    int best = Integer.MIN_VALUE;
-		List<Move> posMoves =  CalcGameTree.calcFreeMoves(nextPlayer(this.copyBoard.playersDraw),copyBoard);
-	    while (posMoves.size() >0)
-	    {
-	    	Board cache = copyBoard.clone();
-	        copyBoard.update(posMoves.get(0));
-	        int wert;
-	        if (PVgefunden)
-	        {
-	            wert = -AlphaBeta(depth-1, -alpha-1, -alpha);
-	            if (wert > alpha && wert < beta) {
-	                wert = -AlphaBeta(depth-1, -beta, -wert);
-	            }
-	        } else {
-	            wert = -AlphaBeta(depth-1, -beta, -alpha);
-	        }
-	        copyBoard = cache;
-	        if (wert > best)
-	        {
-	            if (wert >= beta) {
-	                return wert;
-	            }
-	            best = wert;
-	            if (wert > alpha)
-	            {
-	                alpha = wert;
-	                PVgefunden = true;
-	            }
-	        }
-	    }
-	    return best;
-	}
-}*/
+
+}
+/*
+ * Shallow(Node, Player, Bound) IF Node is terminal, RETURN static value Best =
+ * Shallow(first Child, next Player, Sum) FOR each remaining Child IF
+ * Best[Player] > = Bound, RETURN Best Current = Shallow(Child, next Player, Sum
+ * - Best[Player]) IF Current[Player] > Best[Player], Best = Current RETURN Best
+ * 
+ */
+/**
+ * Basically like wikipedia, except player number is increased
+ * 
+ * @param player
+ * @param depth
+ * @param alpha
+ * @param beta
+ * @return
+ *//*
+	 * int NegaMaxAB(int player, int depth, int alpha, int beta) { List<Move>
+	 * posMoves = copyBoard.calcFreeMoves(player,copyBoard); if (depth == 0 ||
+	 * posMoves == null) return rate2Pl(player); int maxWert = alpha; Board cache;
+	 * for (Move move : posMoves) { cache = copyBoard.clone();
+	 * copyBoard.update(move); int wert = -NegaMaxAB(-player, depth-1, -beta,
+	 * -maxWert); copyBoard = cache; if (wert > maxWert) { maxWert = wert; if
+	 * (maxWert >= beta) break; if (depth == targetDepth) savedMove = move; } }
+	 * return maxWert; }
+	 * 
+	 * /* int AlphaBeta(int depth, int alpha, int beta) { if (depth == 0) return
+	 * rate(); boolean PVgefunden = false; int best = Integer.MIN_VALUE; List<Move>
+	 * posMoves =
+	 * CalcGameTree.calcFreeMoves(nextPlayer(this.copyBoard.playersDraw),copyBoard);
+	 * while (posMoves.size() >0) { Board cache = copyBoard.clone();
+	 * copyBoard.update(posMoves.get(0)); int wert; if (PVgefunden) { wert =
+	 * -AlphaBeta(depth-1, -alpha-1, -alpha); if (wert > alpha && wert < beta) {
+	 * wert = -AlphaBeta(depth-1, -beta, -wert); } } else { wert =
+	 * -AlphaBeta(depth-1, -beta, -alpha); } copyBoard = cache; if (wert > best) {
+	 * if (wert >= beta) { return wert; } best = wert; if (wert > alpha) { alpha =
+	 * wert; PVgefunden = true; } } } return best; } }
+	 */
